628483a1b9b115d59176ad19cd37757dd1cb6a8a
Rizki Abdillah Azmi <rizkiabdillahazmi10@gmail.com>
2023-11-24 10:04:45 +0700

    udpate AuthorizationServerConfig.java

--- src/main/java/com/server/oauth/security/AuthorizationServerConfig.java
+++ src/main/java/com/server/oauth/security/AuthorizationServerConfig.java
@@ -24,7 +24,6 @@ import org.springframework.security.oauth2.server.authorization.*;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.token.*;
 import org.springframework.security.web.SecurityFilterChain;
@@ -47,14 +46,24 @@ public class AuthorizationServerConfig {
             throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 
-        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
-                .oidc(Customizer.withDefaults());
+//        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+//                .oidc(Customizer.withDefaults());
 
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
+
+//    @Bean
+//    public OAuth2TokenGenerator<?> tokenGenerator() throws NoSuchAlgorithmException {
+//        JwtEncoder jwtEncoder = new NimbusJwtEncoder(jwkSource());
+//        JwtGenerator jwtGenerator = new JwtGenerator(jwtEncoder);
+//        OAuth2AccessTokenGenerator accessTokenGenerator = new OAuth2AccessTokenGenerator();
+//        OAuth2RefreshTokenGenerator refreshTokenGenerator = new OAuth2RefreshTokenGenerator();
+//        return new DelegatingOAuth2TokenGenerator(
+//                jwtGenerator, accessTokenGenerator, refreshTokenGenerator);
+//    }
 
     @Bean
     OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer() {
@@ -115,6 +124,45 @@ public class AuthorizationServerConfig {
     @Autowired
     private RsaKeyConfigProperties rsaKeyConfigProperties;
 
+//    @Bean
+//    public JWKSource<SecurityContext> jwkSource() throws NoSuchAlgorithmException {
+//        RSAKey rsaKey = generateRsa();
+//        JWKSet jwkSet = new JWKSet(rsaKey);
+//
+//        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+//    }
+
+//    @Bean
+//    public JWKSource<SecurityContext> jwkSource() throws NoSuchAlgorithmException {
+//        RSAKey rsaKey = generateRsa();
+//        JWKSet jwkSet = new JWKSet(rsaKey);
+//
+//        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+//    }
+
+    private static RSAKey generateRsa() throws NoSuchAlgorithmException {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+        return new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    private static KeyPair generateRsaKey() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+
+        return keyPairGenerator.generateKeyPair();
+    }
+
+
+    //    @Bean
+//    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+//        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+//    }
     @Bean
     public JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withPublicKey(rsaKeyConfigProperties.publicKey()).build();
